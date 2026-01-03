128c5884f5dbe61000f92cf0f2af5d49ca7d97b9
nadarian3 <nadarian3@gmail.com>
2026-01-04 00:06:49 +0100

    Livre.java

--- src/main/java/com/bibliotheque/model/Livre.java
+++ src/main/java/com/bibliotheque/model/Livre.java
@@ -1,62 +1,21 @@
-package com.bibliotheque.model;
+package com.biblio.model;
 
-/**
- * Classe représentant un livre dans la bibliothèque.
- */
 public class Livre extends Document implements Empruntable {
-    private String isbn;
+
     private String auteur;
-    private int anneePublication;
-    private boolean disponible;
-    
-    // Pénalité : 2 DH par jour de retard
-    private static final double PENALITE_PAR_JOUR = 2.0;
+    private boolean disponible = true;
 
-    /**
-     * Constructeur par défaut.
-     */
-    public Livre() {
-        super();
-        this.disponible = true;
-    }
-
-    /**
-     * Constructeur d'un livre.
-     *
-     * @param isbn               l'ISBN du livre (format 978-XXXXXXXXXX)
-     * @param titre              le titre du livre
-     * @param auteur             l'auteur du livre
-     * @param anneePublication   l'année de publication
-     * @param disponible         la disponibilité du livre
-     */
-    public Livre(String isbn, String titre, String auteur, int anneePublication, boolean disponible) {
+    public Livre(String isbn, String titre, String auteur) {
         super(isbn, titre);
-        this.isbn = isbn;
         this.auteur = auteur;
-        this.anneePublication = anneePublication;
-        this.disponible = disponible;
     }
 
-    /**
-     * Constructeur simplifié (disponible par défaut = true).
-     */
-    public Livre(String isbn, String titre, String auteur, int anneePublication) {
-        this(isbn, titre, auteur, anneePublication, true);
+    public String getAuteur() {
+        return auteur;
     }
 
-    /**
-     * Calcule la pénalité de retard pour un livre.
-     * Pénalité : 2 DH par jour de retard.
-     *
-     * @param joursRetard le nombre de jours de retard
-     * @return la pénalité en DH
-     */
-    @Override
-    public double calculerPenaliteRetard(int joursRetard) {
-        if (joursRetard <= 0) {
-            return 0.0;
-        }
-        return joursRetard * PENALITE_PAR_JOUR;
+    public boolean isDisponible() {
+        return disponible;
     }
 
     @Override
@@ -74,48 +33,9 @@ public class Livre extends Document implements Empruntable {
         this.disponible = true;
     }
 
-    // Getters et Setters
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-        setId(isbn); // L'ISBN sert aussi d'ID
-    }
-
-    public String getAuteur() {
-        return auteur;
-    }
-
-    public void setAuteur(String auteur) {
-        this.auteur = auteur;
-    }
-
-    public int getAnneePublication() {
-        return anneePublication;
-    }
-
-    public void setAnneePublication(int anneePublication) {
-        this.anneePublication = anneePublication;
-    }
-
-    public boolean isDisponible() {
-        return disponible;
-    }
-
-    public void setDisponible(boolean disponible) {
-        this.disponible = disponible;
-    }
-
     @Override
-    public String toString() {
-        return "Livre{" +
-                "isbn='" + isbn + '\'' +
-                ", titre='" + getTitre() + '\'' +
-                ", auteur='" + auteur + '\'' +
-                ", annee=" + anneePublication +
-                ", disponible=" + disponible +
-                '}';
+    public double calculerPenaliteRetard(int jours) {
+        return jours * 1.5;
     }
 }
+
