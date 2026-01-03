d565ae0d826da623a71f4abeee98286b018885ab
nadarian3 <nadarian3@gmail.com>
2026-01-04 00:09:44 +0100

    LivreService.java

--- src/main/java/com/bibliotheque/service/LivreService.java
+++ src/main/java/com/bibliotheque/service/LivreService.java
@@ -1,1 +1,26 @@
+package com.biblio.service;
+
+import com.biblio.dao.LivreDAO;
+import com.biblio.dao.impl.LivreDAOImpl;
+import com.biblio.model.Livre;
+
+import java.util.List;
+
+public class LivreService {
+
+    private LivreDAO livreDAO = new LivreDAOImpl();
+
+    public void ajouterLivre(String isbn, String titre, String auteur) {
+        Livre livre = new Livre(isbn, titre, auteur);
+        livreDAO.save(livre);
+    }
+
+    public List<Livre> listerLivres() {
+        return livreDAO.findAll();
+    }
+
+    public void supprimerLivre(String isbn) {
+        livreDAO.delete(isbn);
+    }
+}
 
