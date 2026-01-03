f2b1add67af5e569d3c4b5cadca2bc7860818baf
nadarian3 <nadarian3@gmail.com>
2026-01-04 00:05:51 +0100

    Empruntable.java

--- src/main/java/com/bibliotheque/model/Empruntable.java
+++ src/main/java/com/bibliotheque/model/Empruntable.java
@@ -1,23 +1,8 @@
-package com.bibliotheque.model;
+package com.biblio.model;
 
-/**
- * Interface pour les documents qui peuvent être empruntés.
- */
 public interface Empruntable {
-    /**
-     * Vérifie si le document peut être emprunté.
-     *
-     * @return true si le document est disponible, false sinon
-     */
+
     boolean peutEtreEmprunte();
-
-    /**
-     * Marque le document comme emprunté.
-     */
     void emprunter();
-
-    /**
-     * Marque le document comme retourné.
-     */
     void retourner();
 }
