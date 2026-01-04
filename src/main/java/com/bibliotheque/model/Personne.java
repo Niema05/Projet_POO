a10f2343fc5a32ef2a1a20c8dcee22e8c6d36285
Safaebenhaddou <safae.benhaddou@esi.ac.ma>
2026-01-04 01:32:38 +0100

    Update Personne.java

--- src/main/java/com/bibliotheque/model/Personne.java
+++ src/main/java/com/bibliotheque/model/Personne.java
@@ -1,28 +1,18 @@
-package com.bibliotheque.model;
+package model;
 
-/**
- * Classe abstraite représentant une personne dans le système.
- */
 public abstract class Personne {
     protected String nom;
     protected String prenom;
     protected String email;
 
-    /**
-     * Constructeur de la personne.
-     *
-     * @param nom     le nom de la personne
-     * @param prenom  le prénom de la personne
-     * @param email   l'email de la personne
-     */
-    public Personne(String nom, String prenom, String email) {
+    public Personne() {
+    }
+
+    public Personne( String nom, String prenom, String email) {
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
     }
-
-    // Getters et Setters
-
     public String getNom() {
         return nom;
     }
@@ -30,7 +20,6 @@ public abstract class Personne {
     public void setNom(String nom) {
         this.nom = nom;
     }
-
     public String getPrenom() {
         return prenom;
     }
@@ -38,21 +27,11 @@ public abstract class Personne {
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    /**
-     * Retourne le nom complet (prénom + nom).
-     *
-     * @return le nom complet
-     */
-    public String getNomComplet() {
-        return prenom + " " + nom;
     }
 }
