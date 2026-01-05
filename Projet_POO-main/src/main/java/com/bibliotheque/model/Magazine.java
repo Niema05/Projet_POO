fbf07503bb7d2048332729687e5d3592a69b4a7b
nadarian3 <nadarian3@gmail.com>
2026-01-05 12:57:55 +0100

    Refactor Magazine class and remove comments
    Removed comments and updated the penalty calculation method.

--- Projet_POO-main/src/main/java/com/bibliotheque/model/Magazine.java
+++ Projet_POO-main/src/main/java/com/bibliotheque/model/Magazine.java
@@ -1,35 +1,17 @@
 package com.bibliotheque.model;
 
-/**
- * Classe représentant un magazine dans la bibliothèque.
- */
 public class Magazine extends Document implements Empruntable {
     private int numero;
     private String mois;
     private boolean disponible;
 
-    /**
-     * Constructeur d'un magazine.
-     *
-     * @param id            l'identifiant du magazine
-     * @param titre         le titre du magazine
-     * @param numero        le numéro du magazine
-     * @param mois          le mois de publication
-     * @param disponible    la disponibilité du magazine
-     */
     public Magazine(String id, String titre, int numero, String mois, boolean disponible) {
         super(id, titre);
         this.numero = numero;
         this.mois = mois;
         this.disponible = disponible;
     }
-
-    /**
-     * Calcule la pénalité de retard pour un magazine.
-     * Pénalité : 1 DH par jour de retard.
-     *
-     * @return la pénalité en DH
-     */
+    
     @Override
     public double calculerPenaliteRetard() {
         return 1.0; // 1 DH par jour
@@ -89,7 +71,6 @@ public class Magazine extends Document implements Empruntable {
 
     @Override
     public double calculerPenaliteRetard(int jours) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'calculerPenaliteRetard'");
     }
 }
